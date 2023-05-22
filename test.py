b1343d2ce5f36afb44e47942d8ef97c2febb2d3c
king <1847123212@users.noreply.github.com>
2023-05-22 13:32:31 +0800

    Update test.py
    fix wsdl error

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 import onvif_py
 import time
 
-self = onvif_py.Onvif_hik(ip = "192.168.1.44", port = 80, username = "onvif", password = "abcd1234", wsdl = "'C:\Program Files\Python310\Lib\site-packages\onvif_zeep-0.2.12-py3.10.egg\Lib\site-packages\wsdl'")
+self = onvif_py.Onvif_hik(ip = "192.168.1.44", port = 80, username = "onvif", password = "abcd1234", wsdl = "C:\Program Files\Python310\Lib\site-packages\onvif_zeep-0.2.12-py3.10.egg\Lib\site-packages\wsdl")
 if self.content_cam() == True :
     start = time.time()
     for i in range(1,10):
