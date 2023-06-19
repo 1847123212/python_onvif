a121d5d518d01a14c80b6a75427383bde866bdaa
king <1847123212@users.noreply.github.com>
2023-06-19 19:15:56 +0800

    Update test.py
    when go from two presets cause very long time, should add delay time
    add get preset when cll preset

--- test.py
+++ test.py
@@ -1,11 +1,13 @@
 import onvif_py
 import time
 
-self = onvif_py.Onvif_hik(ip = "192.168.1.30", port = "8080", username = "admin", password = "abcd1234", wsdl = "C:\Program Files\Python310\Lib\site-packages\onvif_zeep-0.2.12-py3.10.egg\Lib\site-packages\wsdl")
+self = onvif_py.Onvif_hik(ip = "192.168.1.30", port = "80", username = "admin", password = "abcd1234", wsdl = "C:\Program Files\Python310\Lib\site-packages\onvif_zeep-0.2.12-py3.10.egg\Lib\site-packages\wsdl")
 if self.content_cam() == True :
+    self.get_presets()
     print('go to preset 1')
     self.goto_preset(1)
-    time.sleep(5)
+    time.sleep(10)
+    print('arrive preset 1')
     start = time.time()
     for i in range(1,10):
         self.Snapshot()
@@ -13,7 +15,17 @@ if self.content_cam() == True :
     print('time usage: ', (end - start) * 100, 'ms')
     print('go to preset 2')
     self.goto_preset(2)
-    time.sleep(5)
+    time.sleep(10)
+    print('arrive preset 2')
+    start = time.time()
+    for i in range(1,10):
+        self.Snapshot()
+    end = time.time()
+    print('time usage: ', (end - start) * 100, 'ms')
+    print('go to preset 3')
+    self.goto_preset(3)
+    time.sleep(10)
+    print('arrive preset 3')
     start = time.time()
     for i in range(1,10):
         self.Snapshot()
