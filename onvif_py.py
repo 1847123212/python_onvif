bf9628818d3d99348e9934cc3972248eb0a33112
king <1847123212@users.noreply.github.com>
2023-06-19 11:25:29 +0800

    Update onvif_py.py
    add preset code

--- onvif_py.py
+++ onvif_py.py
@@ -25,6 +25,7 @@ class Onvif_hik(object):
             self.mycam = ONVIFCamera(self.ip, self.port, self.username, self.password, self.wsdl) # WSDL文件目录不同的操作系统以及安装目录是不一样的，注意替换
             self.media = self.mycam.create_media_service()  # 创建媒体服务
             self.media_profile = self.media.GetProfiles()[0]  # 获取配置信息
+            self.ptz = self.mycam.create_ptz_service()  # 创建控制云台服务
             return True
         except Exception as e:
             return False
@@ -35,10 +36,29 @@ class Onvif_hik(object):
         :return: 
         """
         res = self.media.GetSnapshotUri({'ProfileToken': self.media_profile.token})
-
         response = requests.get(res.Uri, auth=HTTPDigestAuth(self.username, self.password))
         with open(self.save_path, 'wb') as f:  # 保存截图
             f.write(response.content)
+
+    def get_presets(self):
+        """
+        获取预置点列表
+        :return:预置点列表--所有的预置点
+        """
+        presets = self.ptz.GetPresets({'ProfileToken': self.media_profile.token})  # 获取所有预置点,返回值：list
+        return presets
+
+    def goto_preset(self, presets_token: int):
+        """
+        移动到指定预置点
+        :param presets_token: 目的位置的token，获取预置点返回值中
+        :return: 
+        """
+        try:
+            self.ptz.GotoPreset(
+                {'ProfileToken': self.media_profile.token, "PresetToken": presets_token})  # 移动到指定预置点位置
+        except Exception as e:
+            print(e)
  
     def zeep_pythonvalue(self, xmlvalue):
         return xmlvalue
